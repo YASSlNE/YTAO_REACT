09e5a05981409ac5555f9f5ff63adcd558ee8913
YASSlNE <mdyassinechiboub@gmail.com>
2022-08-07 18:09:35 +0100

    deploying

---
+++ src/components/Home.tsx
@@ -0,0 +1,35 @@
+import {useState} from 'react'
+import axios from 'axios'
+function Home( ) {
+  const [Yurl, SetYurl] = useState();
+  const [Aurl, SetAurl] = useState();
+  // componentDidMount axios() {
+  //   await axios.get(`https://jsonplaceholder.typicode.com/users`)
+  //     .then(res => {
+  //       const persons = res.data;
+  //       this.setState({ persons });
+  const getUrl = async() =>{
+    // SetYurl()
+    await axios.get(`https://ytaoapp.herokuapp.com/api?url=${Yurl}`)
+      .then(res => {
+        SetAurl(res['data']['url']); 
+      }
+      )
+  }
+  const onChange = (e : any)=>{
+    SetYurl(e.target.value)
+  }
+  return (
+    <>
+    <audio controls preload="auto" key={Aurl} >
+            <source src={Aurl} type="audio/mpeg" /> 
+    </audio>
+    <input type="text" value={Yurl} onChange={onChange} placeholder='Put the youtube link here' />
+    <button onClick={getUrl}>Get audio</button>
+    </>
+  )
+}
+
+
+
+export default Home
